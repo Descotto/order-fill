372f725b0ce5af460d3f73e16aabdacf64dd5798
Descotto <davidescotto@hotmail.com>
2023-05-13 02:33:28 -0700

    setup react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <from>
+        <input type="text" placeholder="order number" />
+        <button type="submit">submit</button>
+      </from>
+
+    </div>
+  );
+}
+
+export default App;
