1709315a90962fcfbf1ac26ac4b8d35aceed7b6c
Descotto <davidescotto@hotmail.com>
2023-05-17 19:08:07 -0700

    added display for empty orders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Order from './Order.js';
+import Display from './Display.js';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <from>
-        <input type="text" placeholder="order number" />
-        <button type="submit">submit</button>
-      </from>
+  const [order, setOrder] = useState();
 
+
+  return (
+    <div>
+      <Router>
+        <nav id="navbar" className="navbar">
+          <ul>
+            <li><Link className="nav-link active" to="/">Home</Link></li>
+          </ul>
+          <i className="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<Order setOrder={setOrder} />} />
+          <Route path='/display' element={<Display order={order} />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
